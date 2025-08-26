82131977f8bf1ffbf25bad92b85982b12c70ea27
A-Brusnitsyn <al.brusnitsyn@yandex.ru>
2025-08-26 20:36:48 +0500

    Решение задачи 2

--- src/HomeWorks/HomeWork4.java
+++ src/HomeWorks/HomeWork4.java
@@ -1,6 +1,7 @@
 package HomeWorks;
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class HomeWork4 {
@@ -15,22 +16,44 @@ public class HomeWork4 {
 
         System.out.println("Введите размер массива");
         int arraySize = scanner.nextInt();
-        int [] numbers = new int[arraySize];
+        int[] numbers = new int[arraySize];
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i]=(int) (Math.random()*100);
+            numbers[i] = (int) (Math.random() * 100);
         }
-    /*Задача 1:
-    Пройти по массиву, вывести все элементы в прямом и в обратном порядке.*/
+        /*Задача 1:
+        Пройти по массиву, вывести все элементы в прямом и в обратном порядке.*/
         System.out.println("Задача 1:");
         System.out.println("Массив в прямом порядке:");
-        for (int i:numbers){
+        for (int i : numbers) {
             System.out.println(i);
         }
         System.out.println("И в обратном:");
-        for (int i = numbers.length-1; i >=0 ; i--) {
+        for (int i = numbers.length - 1; i >= 0; i--) {
             System.out.println(numbers[i]);
+        }
+        scanner.close();
+        /*Задача 2:
+        Найти минимальный-максимальный элементы и вывести в консоль*/
+        System.out.println("Задача 2:");
+        System.out.println("Вариант 1:");
+        int max = numbers[0];
+        int min = numbers[0];
+        for (int i = 1; i < numbers.length ; i++) {
+            if (numbers[i]> max){
+                max=numbers[i];
+            }
+            if (numbers[i]<min){
+                min=numbers[i];
+            }
 
         }
+        System.out.println("Минимальное значение:" + min + " Максимальное значение: " + max);
+
+        System.out.println("Вариант 2:");
+        Arrays.sort(numbers);
+        System.out.println("Минимальное значение массива: " + numbers[0]);
+        System.out.println("Максимальное значение массива: " + numbers[numbers.length-1]);
+
     }
 
 }
