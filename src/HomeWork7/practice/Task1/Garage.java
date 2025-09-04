38a6110ebcda83ec1988ea009afc950cfbb33814
A-Brusnitsyn <al.brusnitsyn@yandex.ru>
2025-09-04 20:55:30 +0500

    Практика 7 урок, задача 1.

---
+++ src/HomeWork7/practice/Task1/Garage.java
@@ -0,0 +1,7 @@
+package HomeWork7.practice.Task1;
+
+public class Garage extends Build {
+    protected Garage(String type) {
+        super(type);
+    }
+}
---
+++ src/HomeWork7/practice/Task1/House.java
@@ -0,0 +1,7 @@
+package HomeWork7.practice.Task1;
+
+public class House extends Build {
+    protected House(String type) {
+        super(type);
+    }
+}
