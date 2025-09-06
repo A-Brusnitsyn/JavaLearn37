dd46895c1bbd21165aed698caf7e3ba758ddf190
A-Brusnitsyn <al.brusnitsyn@yandex.ru>
2025-09-06 13:33:14 +0500

    Домашка 7, задача 1

---
+++ src/HomeWork7/homeWork/Task1/Doctor.java
@@ -0,0 +1,13 @@
+package HomeWork7.homeWork.Task1;
+
+public class Doctor {
+    protected String profession;
+    protected String[] heal;
+
+    public Doctor(String profession) {
+    }
+
+    void heal(Patient patient) {
+    }
+
+}
