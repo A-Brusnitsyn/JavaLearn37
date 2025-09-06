1830623f288986b159f5ffbbae9dcaf5e41a07ec
A-Brusnitsyn <al.brusnitsyn@yandex.ru>
2025-09-06 15:31:49 +0500

    Домашка 7, задача 2

---
+++ src/HomeWork7/homeWork/Task2/Apple.java
@@ -0,0 +1,8 @@
+package HomeWork7.homeWork.Task2;
+
+public class Apple {
+    private String color = "green";
+    public void showColor(){
+        System.out.println(color);
+    }
+}
---
+++ src/HomeWork7/homeWork/Task2/Main.java
@@ -0,0 +1,24 @@
+package HomeWork7.homeWork.Task2;
+
+import java.lang.reflect.Field;
+
+public class Main {
+    public static void main(String[] args) {
+        /**Задача *:
+         Создать класс Apple и добавить в него поле color с модификатором доступа private и
+         инициализировать его. В методе main другого класса создать объект Apple, и не
+         используя сеттеры изменить значение поля color.*/
+
+        try {
+            Apple apple = new Apple();
+            apple.showColor();
+            Field field = Apple.class.getDeclaredField("color");
+            field.setAccessible(true);
+            field.set(apple, "yellow");
+            apple.showColor();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
