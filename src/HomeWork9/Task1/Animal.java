250da9db8e647b7529b0660a9528d746eae1f91c
A-Brusnitsyn <al.brusnitsyn@yandex.ru>
2025-09-11 21:32:17 +0500

    HomeWork 9, Task 1

---
+++ src/HomeWork9/Task1/Animal.java
@@ -0,0 +1,21 @@
+package HomeWork9.Task1;
+
+public class Animal {
+    private String name;
+    private String food;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+    public Animal(){
+
+    }
+
+    public void voice(){
+
+    }
+
+    public void eat(String food){
+
+    }
+}
