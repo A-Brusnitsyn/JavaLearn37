0b1bc11100aec993f0afbe186d9c08d5f4a6779c
A-Brusnitsyn <al.brusnitsyn@yandex.ru>
2025-09-14 16:50:47 +0500

    HomeWork 10, Task 1, Star

---
+++ src/HomeWork10/Link.java
@@ -0,0 +1,24 @@
+package HomeWork10;
+
+public class Link {
+    private String url;
+
+    public Link(String url){
+        this.url=url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "Link{" +
+                "url='" + url + '\'' +
+                '}';
+    }
+}
